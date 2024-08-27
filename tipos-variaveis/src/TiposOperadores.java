abd661f3f375ff8ac36343e053943b160405bfa2
Vincius Von <vini.r.faria@hotmail.com>
2024-08-27 15:15:19 -0300

    treino de condições e operadores booleanos

--- tipos-variaveis/src/TiposOperadores.java
+++ tipos-variaveis/src/TiposOperadores.java
@@ -1,5 +1,24 @@
 public class TiposOperadores {
-    public static void main(String[] args) {
+   
+  public static void main(String[] args) {
+    
+        boolean condicao1 = true;
+
+        boolean condicao2 = true;
+
+        if (condicao1 && (7 > 4)) {
+            System.out.println("as duas condições são verdadeiras");
+        }
+       
+        if (condicao1 || condicao2) {
+            System.out.println(" uma condições são verdadeiras");
+        }
+
+
+        System.out.println("fim");
+  } 
+   
+    /* public static void main(String[] args) {
         
         int a, b;
 
@@ -9,8 +28,5 @@ public class TiposOperadores {
         String resultado = a==b ?"verdadeiro" : "falso";
     
         System.out.println(resultado);
-
-
-
-    }
+    }*/
 }
